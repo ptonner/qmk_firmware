9b343364b04ba68998a18abfd978fde66bf608a7
Peter Tonner <peter.tonner@gmail.com>
2019-03-08 21:30:32 -0500

    some changes

--- keyboards/planck/keymaps/ptonner/keymap.c
+++ keyboards/planck/keymaps/ptonner/keymap.c
@@ -41,7 +41,9 @@ enum planck_keycodes {
 
 enum tapdances{
       TD_LSFT_CAPS = 0,
-      TD_RESET = 1
+      TD_RSFT_CAPS,
+      TD_RESET,
+      TD_AG_SWAP
 };
 
 void safe_reset(qk_tap_dance_state_t *state, void *user_data) {
@@ -54,6 +56,8 @@ void safe_reset(qk_tap_dance_state_t *state, void *user_data) {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+  [TD_AG_SWAP] = ACTION_TAP_DANCE_DOUBLE(_______, AG_SWAP),
   [TD_RESET] = ACTION_TAP_DANCE_FN(safe_reset)
 };
 
@@ -78,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    TD(TD_LSFT_CAPS),  KC_LEAD, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RGUI, KC_RALT, KC_LBRC, KC_RBRC
-),
+    TD(TD_LSFT_CAPS),  KC_LEAD, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RALT, KC_LBRC, KC_RBRC, TD(TD_RSFT_CAPS)
+			       ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -130,9 +134,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, TD(TD_RESET), DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______,   MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
-    _______, MUV_DE,    MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    _______, _______,      MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______,  _______,
+    _______, MUV_DE,       MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+    _______, _______,      _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 
 /* Colemak
